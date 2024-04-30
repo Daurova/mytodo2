192d7eb03abe6b3af0a6843bee84942db784f1e7
Daurova <147259989+Daurova@users.noreply.github.com>
2024-05-01 00:42:50 +0300

    0105 0042

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,11 +1,11 @@
-const reportWebVitals = onPerfEntry => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
+const reportWebVitals = onPerEntry => {
+  if (onPerEntry && onPerEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry)
-      getFID(onPerfEntry)
-      getFCP(onPerfEntry)
-      getLCP(onPerfEntry)
-      getTTFB(onPerfEntry)
+      getCLS(onPerEntry)
+      getFID(onPerEntry)
+      getFCP(onPerEntry)
+      getLCP(onPerEntry)
+      getTTFB(onPerEntry)
     })
   }
 }
