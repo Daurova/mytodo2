d2c5eb0af5d48104e30d82f2519c01536c43dc00
Daurova <daurova@yandex.ru>
2024-06-01 12:20:58 +0300

    0105 1220

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,20 +1,55 @@
+import React from 'react';
 import './Task.css'
 
 
-const Task = ({ description, completed, created, onEdit, onDelete }) => {
-    return(
-        <li className={completed ? "completed" : ""}>
-          <div className="view">
-            <input className="toggle" type="checkbox" checked={completed}></input>
-            <label>
-              <span className="description">{description}</span>
-              <span className="created">{created}</span>
-            </label>
-            <button className="icon icon-edit" onClick={onEdit}></button>
-            <button className="icon icon-destroy" onClick={onDelete}></button>
-          </div>
+
+class Task extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {date: new Date()}
+  }
+  componentDidMount(){
+    this.timerID = setInterval(()=>this.tick(), 1000) 
+   }
+  
+  componentWillUnmount(){
+    clearInterval(this.timerID)
+  }
+  
+  tick(){
+    this.setState({date: new Date()})
+  }
+  
+
+
+
+  render() {
+    const { description, completed, created, onEdit, onDelete } = this.props;
+
+
+
+    return (
+      <li className={completed ? "completed" : ""}>
+        <div className="view">
+          <input
+            className="toggle"
+            type="checkbox"
+            checked={completed}
+            readOnly
+          />
+          <label>
+            <span className="description">{description}</span>
+            <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+            <span className="created">{created}</span>
+          </label>
+          <button className="icon icon-edit" onClick={onEdit}></button>
+          <button className="icon icon-destroy" onClick={onDelete}></button>
+        </div>
       </li>
-    )
+    );
+  }
 }
+
+
 
 export default Task;
