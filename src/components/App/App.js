95329c0ecd69354e8a8fb511e6f276945e550552
Daurova <daurova@yandex.ru>
2024-06-13 13:20:23 +0300

    1306 1320

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,12 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { formatDistanceToNow } from 'date-fns'
 
 import Footer from '../Footer'
 import TaskList from '../TaskList/TaskList'
 import NewTaskForm from '../../components/NewTaskForm'
 
-import '../../index'
-
-class App extends Component {
-  maxId = 100
-  editedTasks = []
-  doneTasks = []
-  typeTasks = 'all'
-  initialTasks = [
+const App = () => {
+  const [tasks, setTasks] = useState([
     {
       id: 1,
       description: 'task 1',
@@ -48,168 +42,119 @@ class App extends Component {
       createdDate: new Date(),
       completed: false,
     },
-  ]
+  ])
+  const [description, setDescription] = useState('')
+  const [typeTasks, setTypeTasks] = useState('all')
 
-  state = {//changed
-    tasks: [...this.initialTasks],
-    filteredTasks:[...this.initialTasks],
-    isFilterActive: false,
-    typeTasks: 'all'
-  }
-
-  onEditTask = (id) => {
-    console.log('edit ', id)
-    const updatedTasks = this.state.tasks.map((task) => {
+  const onEditTask = (id) => {
+    const updatedTasks = tasks.map((task) => {
       if (task.id === id) {
         return {
           ...task,
-          isEditing: true, // добавляем флаг для редактирования
+          isEditing: true,
         }
       }
       return task
     })
 
-    this.setState({ tasks: updatedTasks })
-   
+    setTasks(updatedTasks)
   }
 
-  
-  onCompletedTask = (id) => {
-    console.log('complete', id)
-    this.setState (({ tasks })=>{
-      const updatedTasks = tasks.map((task) => {
-        if (task.id === id) {
-          return {
-            ...task,
-            completed: !task.completed,
-          }
+  const onCompletedTask = (id) => {
+    const updatedTasks = tasks.map((task) => {
+      if (task.id === id) {
+        return {
+          ...task,
+          completed: !task.completed,
         }
-        return task
-      })
-  
-      return { tasks: updatedTasks}
-
+      }
+      return task
     })
-  } 
 
-  onDeleteTask = (id) => {
-    console.log('delete ', id)
-    this.setState(({ tasks }) => {
-      const idx = tasks.findIndex((el)=> el.id ===id)
-      const newTasks = [
-        ...tasks.slice(0, idx),
-        ...tasks.slice(idx + 1)
-      ]
-      return { tasks: newTasks }
-    })
+    setTasks(updatedTasks)
   }
 
-  completed = (id) => {
-    console.log(id)  }
-  
-  onInputChange = (event) => {
-    console.log(event.target.value)
-    this.setState({
-      description: event.target.value
-    })
+  const onDeleteTask = (id) => {
+    const newTasks = tasks.filter(task => task.id !== id)
+    setTasks(newTasks)
   }
-   
-  onSubmit = (event) => {
-    console.log('submit')
+
+  const completed = (id) => {
+    console.log(id)
+  }
+
+  const onInputChange = (event) => {
+    setDescription(event.target.value)
+  }
+
+  const onSubmit = (event) => {
     event.preventDefault()
-    this.onItemAdded(this.state.description)
-    this.setState({
-      description: ''
-    })
+    onItemAdded(description)
+    setDescription('')
   }
 
-
-  getTasksToDoCount = () => {
-    return this.state.tasks.filter(task => !task.completed).length
+  const getTasksToDoCount = () => {
+    return tasks.filter(task => !task.completed).length
   }
-  onItemAdded=(description)=>{
-    console.log('item added')
-    const newItem ={
-      id: this.maxId++,
+
+  const onItemAdded = (description) => {
+    const newItem = {
+      id: tasks.length + 1,
       description: description,
       created: 'created 5 minutes ago',
       createdDate: new Date(),
       completed: false
     }
 
-    this.setState(({tasks})=>{
-      const newArr=[
-        ...tasks,
-        newItem
-      ]
-      return {
-        tasks: newArr
-      }
-    }
-    )
+    setTasks([...tasks, newItem])
     console.log(formatDistanceToNow(new Date()))
   }
 
-  clearCompletedTasks = () => {
-    const updatedTasks = this.state.tasks.filter(task => !task.completed)
-    this.setState({ tasks: updatedTasks })}
-
-  onAll=()=>{
-    console.log('onAll')
-    this.setState({ typeTasks: 'all' })
-  }
-  onActive=()=>{
-    console.log('onActive')
-    this.setState({ typeTasks: 'active' })
-  }
-  onDone=()=>{
-    console.log('onDone')
-    this.setState({ typeTasks: 'done' })
+  const clearCompletedTasks = () => {
+    const updatedTasks = tasks.filter(task => !task.completed)
+    setTasks(updatedTasks)
   }
 
-render() {
-    return (
-      <div className='todoapp'>
-        <NewTaskForm
-          onInputChange={this.onInputChange}
-          onSubmit = {this.onSubmit}
-          value={this.state.description}
-        />
-        <TaskList 
-          tasks={this.state.tasks}
-          onEditTask={this.onEditTask} 
-          onDeleteTask={this.onDeleteTask}
-          completed = {this.completed} 
-          onCompletedTask={this.onCompletedTask}
-          typeTasks={this.state.typeTasks}
-        />
-        <Footer
-          tasksToDoCount = {this.getTasksToDoCount}
-          onAll={this.onAll}
-          onActive={this.onActive}
-          onDone={this.onDone}
-          clearCompletedTasks={this.clearCompletedTasks}
-        />
-      </div>
-    )
+  const onAll = () => {
+    setTypeTasks('all')
   }
-}
 
-App.defaultProps = {
-  tasks: [], 
-  filteredTasks: [], 
-  onEditTask:() => {},
-  onCompletedTask:() => {},
-  onDeleteTask:() => {},
-  onInputChange:() => {},
-  onSubmit:() => {},
-  getTasksToDoCount:() => {},
-  onItemAdded:() => {},
-  clearCompletedTasks:() => {},
-  onAll:() => {},
-  onActive:() => {},
-  onDone:() => {}
+  const onActive = () => {
+    setTypeTasks('active')
+  }
+
+  const onDone = () => {
+    setTypeTasks('done')
+  }
+
+  return (
+    <div className='todoapp'>
+      <NewTaskForm
+        onInputChange={onInputChange}
+        onSubmit={onSubmit}
+        value={description}
+      />
+      <TaskList
+        tasks={tasks}
+        onEditTask={onEditTask}
+        onDeleteTask={onDeleteTask}
+        completed={completed}
+        onCompletedTask={onCompletedTask}
+        typeTasks={typeTasks}
+      />
+      <Footer
+        tasksToDoCount={getTasksToDoCount}
+        onAll={onAll}
+        onActive={onActive}
+        onDone={onDone}
+        clearCompletedTasks={clearCompletedTasks}
+      />
+    </div>
+  )
 }
 
 export default App
+
+
+
 
